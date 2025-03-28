bcba049a33e1c12c245bac3a9eb44b8418d1bfa5
Driek <hello@drieq.com>
2025-03-28 20:36:44 +0000

    PA to local

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('sign_up/', views.sign_up, name='sign_up'),
 ]
